564122d6e4a46085c90f736bd06b895b4fd3f06c
Alison Woodman <aliwoodman@gmail.com>
2017-07-27 11:35:59 +0100

    Passing test for filtering by tag

--- spec/features/filtering_by_tag_spec.rb
+++ spec/features/filtering_by_tag_spec.rb
@@ -2,9 +2,9 @@ feature 'FEATURE: filtering by tag' do
   scenario 'the user can view only links with a certain tag' do
     submit_test_link('www.google.com', 'Google', 'Browser')
     submit_test_link('www.bubbles.com', 'Bubbly', 'Bubbles')
-    visit 'tags/bubbles'
+    visit 'tags/Bubbles'
     expect(page.status_code).to eq(200)
     expect(page).to have_content('Bubbles')
-    expect(page).to_not have_content('Google')
+    expect(page).to_not have_content('Browser')
   end
 end
