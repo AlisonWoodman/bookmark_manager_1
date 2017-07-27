d36e5bfcd236e0465444496107ef22832039caeb
Alison Woodman <aliwoodman@gmail.com>
2017-07-27 11:53:11 +0100

    Adding multiple tags, feature test pass

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,8 +1,14 @@
 require_relative 'web_helpers'
 
-feature 'FEATURE: adding tags' do
+feature 'FEATURE: adding one tag' do
   scenario 'the user can add tags to a link' do
     submit_test_link('www.google.com', 'Google', 'Browser')
     expect(page).to have_content('Browser')
+  end
+
+  scenario 'the user can add mulitple tags to a link' do
+    submit_test_link('www.google.com', 'Google', 'Browser, Work')
+    expect(page).to have_content('Browser')
+    expect(page).to have_content('Work')
   end
 end
